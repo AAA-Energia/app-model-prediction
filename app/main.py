15fc964e38d373962d32a9700c6b8c37c41c5d9c
mariooef <mefcareerpath@gmail.com>
2024-06-01 17:32:26 -0700

    Modificaciones en codigo para corregir lo del gradiente boosting

--- app/main.py
+++ app/main.py
@@ -25,18 +25,44 @@ def load_model(model_name, model_stage=None):
     except MlflowException as e:
         st.error(f"Error al cargar el modelo: {e}")
         return None
+    
+def validate_and_adjust_data(data):
+    try:
+        # Convertir todos los datos a tipo float64
+        data = data.astype('float64')
+        return data
+    except Exception as e:
+        st.error(f"Error al convertir los datos: {e}")
+        return None
+
+def read_example_csv():
+    example_file_path = "data/test.csv"
+    example_df = pd.read_csv(example_file_path)
+    return example_df
 
 # Interfaz de usuario de Streamlit
 st.title("Prueba de Modelos en Producción")
 
 def main():
-    # Subir archivo CSV con datos de prueba
-    model_name = st.radio("Seleccione un modelo", ["Prophet_Model","Gradient_Boosting_Model","random_forest_model"], index = 2)
+    st.write("1. Seleccione un modelo")
+    model_name = st.radio("", ["Prophet_Model","Gradient_Boosting_Model","random_forest_model"], index = 2)
     #model_stage = st.selectbox("Stage del modelo", ["Production", "Staging", "None"])
     st_frame = st.empty()
 
     if model_name != "Prophet_Model":
-        uploaded_file = st.file_uploader("Subir archivo CSV con datos de prueba", type=["csv"])
+        
+        st.write("2. Descargue el archivo y luego llenelo con la información debida")
+        example_df = read_example_csv()
+        example_csv = example_df.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Descargar archivo CSV de ejemplo",
+            data=example_csv,
+            file_name='example.csv',
+            mime='text/csv'
+        )
+        
+        st.write("3. Subir archivo CSV con datos segun el archivo de ejemplo")
+        uploaded_file = st.file_uploader("", type=["csv"])
         if uploaded_file is not None:
             data = pd.read_csv(uploaded_file)
             st.write("Datos de prueba:")
@@ -45,12 +71,26 @@ def main():
             #model_name = st.text_input("Nombre del modelo", "Prophet_Model")
             #model_stage = st.selectbox("Stage del modelo", ["Production", "Staging", "None"])
 
-            if st.button("Cargar Modelo"):
+            if st.button("Correr Modelo"):
                 model = load_model(model_name)
                 st.success(f"Modelo {model_name} cargado exitosamente.")
-                predictions = model.predict(data)
-                st.write("Predicciones:")
-                st.write(predictions)
+                #predictions = model.predict(data)
+                #st.write("Predicciones:")
+                #st.write(predictions)
+
+                valid_data = validate_and_adjust_data(data)
+
+                if valid_data is not None:
+                    #st.write("Datos de prueba después de la validación y ajuste:")
+                    #st.write(valid_data)
+
+                    try:
+                        predictions = model.predict(valid_data)
+                        st.write("Predicciones:")
+                        st.write(predictions)
+                    except Exception as e:
+                        st.error(f"Error al ejecutar las predicciones: {e}")
+
                 #if model:
                     
                 #if st.button("Ejecutar predicciones"):
